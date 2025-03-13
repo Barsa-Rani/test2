5990d6fd5079d65a06ef5abbda30c83508f65518
Barsa-Rani <nayakbarsarani2003@gmail.com>
2025-03-13 11:54:32 +0530

    Update Test69Application.java

--- test69/test69/src/main/java/com/test69/Test69Application.java
+++ test69/test69/src/main/java/com/test69/Test69Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test69Application {
 
 	public static void main(String[] args) {
+		int y=23;
 		SpringApplication.run(Test69Application.class, args);
 	}
 
